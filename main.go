2af939bc43e6e8790e0ed4a71f940d34e615bc3b
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2023-01-03 09:39:35 +0100

    Better Commented

--- main.go
+++ main.go
@@ -1,44 +1,42 @@
+//FIRST LINE
 package main
-//CREATING A WEBSERVER IS GO
+//IMPORT ALL THE PACKAGES
 import(
-	"fmt"
-	"log"
-	"net/http"
+	"fmt"//BASICALLY A CONSOLE.LOG
+	"log"//TO LOG OUT ERRORS AND STUFF
+	"net/http"//HTTP METHOD
 )
-//FUNCTION TO HANDLE FORM.html
-func formHandler(w http.ResponseWriter,r *http.Request){
-	if err:= r.ParseForm(); err!= nil{
-		fmt.Fprintf(w,"ParseForm() err: %v",err)
-		return
+//FUNCTION TO HANDLE THE INDEX.html FILE
+func indexHandler(w http.ResponseWriter, r *http.Request){//BASICALLY A (RES,REQ) => LIKE IN NODEJS
+	if(r.URL.Path != "/hello"){//CONDITIONAL FOR PATH
+     http.Error(w,"NOT FOUND",http.StatusNotFound);//ERROR CODE
 	}
-	fmt.Fprintf(w,"POST REQUEST SUCCESSFUL")
-	name := r.FormValue("name")
-	address := r.FormValue("address")
-	fmt.Fprintf(w, "Name = %s\n",name);
-	fmt.Fprintf(w, "Address = %s\n",address);
-}
-//FUNCTION TO HANDLE INDEX.html
-func helloHandler(w http.ResponseWriter,r *http.Request){
-if(r.URL.Path != "/hello"){
-	http.Error(w,"404 not Found",http.StatusNotFound)
-	return
-}
-if(r.Method != "GET"){
-	http.Error(w,"method is not supported",http.StatusNotFound);
-	return
-}
-fmt.Fprintf(w,"hello")
-} 
-
-//MAIN SERVER CODE
-func main(){
-	fileServer := http.FileServer(http.Dir("./static"));
-	http.Handle("/",fileServer)
-	http.HandleFunc("/form",formHandler);
-	http.HandleFunc("/hello",helloHandler);
-	fmt.Println("STARTING SERVER @ PORT 8080");
-	if err := http.ListenAndServe(":8080",nil); err!=nil {
-		 log.Fatal((err));
+	if(r.Method != "GET"){//CONDITIONAL FOR THE METHOD
+		http.Error(w,"NOT FOUND",http.StatusNotFound);
 	}
+	fmt.Fprintf(w,"HELLO")//PRINTS OUT HELLO ON THE PAGE
 	
-} 
+}
+func formHandler(w http.ResponseWriter, r *http.Request){
+	if err:= r.ParseForm();err != nil{//TO PARSE OUT DATA FROM THE FORM
+		fmt.Fprintf(w,"ERROR AT FORM  %v",err)
+	}
+	fmt.Printf("POST REQUEST SUCCESSFUL\n");
+	name := r.FormValue("name");//READING OUT NAME VALUE FROM THE FORM
+	address := r.FormValue("address");//READING OUT ADDRESS VALUE FROM THE FORM
+	fmt.Fprintf(w,"NAME IS %v\n",name);//PRINTS OUT THE NAME VALUE
+	fmt.Fprintf(w,"ADDRESS IS %v",address);//PRINTS OUT THE ADDRESS VALUE
+
+}
+func main(){
+fileServer := http.FileServer(http.Dir("./static"));//INITIALIZE A VARIABLE FILESERVER WITH THE STATIC FILES TO BE SERVE
+http.Handle("/",fileServer);//USES THE PATH + FILES IE(APP.USE("/",API)) --- EXPRESS JS
+http.HandleFunc("/hello",indexHandler);//HANDLES THE PATH /HELLO 
+http.HandleFunc("/form",formHandler);//HANDLES THE PATH FORM
+fmt.Printf("SERVER IS LIVE @ PORT 3000");
+if err:= http.ListenAndServe(":3000",nil);err!=nil{//USES A PORT 
+	log.Fatal((err))
+}
+
+
+}
